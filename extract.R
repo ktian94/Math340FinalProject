052b4157c7e24dffcf2ab759a72e52b900481b54
ktian94 <kt493@georgetown.edu>
2015-05-01 12:11:31 -0400

    Cleaned up extract.R

--- extract.R
+++ extract.R
@@ -1,8 +1,7 @@
-# Kevin Tian, Naveen Yarlagadda, Taylor Perz
-
-#loading necessary libraries and packages
-library(MASS)
-library(tree)
+# File-Name:       extract.R           
+# Date:            2015-05-01                                
+# Author:          Kevin Tian, Taylor Perz, Naveen Yarlagadda                             
+# Purpose:         Extraction base code for experiments
 
 # load data into R
 fert_rates <- read.csv("adoFertility/sp.ado.tfrt_Indicator_en_csv_v2.csv", header=TRUE)
@@ -38,19 +37,14 @@ uam_13.clean <- uam_13[complete.cases(uam_13),]
 fpp_13.clean <- fpp_13[complete.cases(fpp_13),]
 gdp_13.clean <- gdp_13[complete.cases(gdp_13),]
 
-# uaf - 206, hiv - 128, gdp - 218, fr - 227
-
-#histrogram of the distribution of the fr_13 in order to determine a high 
-#and low fertility rate 
+# transform fertility rate into a categorical variable based off histogram
 hist(fr_13.clean$X2013)
-summary(fr_13.clean$X2013)
-
-
-#demo how to make a continuous variable into a categorical one
 low_fr_13 <- ifelse(fr_13.clean$X2013<=60,1,0)
 fr_13.class <- cbind(fr_13.clean,low_fr_13)
 
-# merge
+# clean counts: uaf/uam - 206, gdp - 218, fr - 227
+
+# merge uaf, uam, gdp, and fr into one table
 ua_13 <- merge(uaf_13.clean,uam_13.clean[,c(1,5)],by='Country.Name')
 names(ua_13)[5] <- "uaf_2013"
 names(ua_13)[6] <- "uam_2013"
@@ -60,89 +54,7 @@ names(ua_gdp_13)[8] <- "gdp_2013"
 final_13 <- merge(ua_gdp_13,fr_13.class[,c(1,5:6)],by='Country.Name')
 names(final_13)[9] <- "fr_2013"
 
-#dividing into training and testing for the New Data Set 
-tr<-sample(seq(1,nrow(final_13)),98,replace=FALSE)
-final_13.tr<-final_13[tr,]
-final_13.ts<-final_13[-tr,]
-
-#doing the classification analysis with the New Data Set 
-fr_13.lda<-lda(low_fr_13~uaf_2013 + uam_2013 + gdp_2013 ,data=final_13.tr)
-
-#results of the classification analysis with predictors unemployment variables male and female and gdp 
-fr_13.lda
-
-#doing plots
-plot(fr_13.lda)
-plot(fr_13.lda, dimen=1,type="density")
-
-#doing the predictions (for some reason this part does not work) 
-fr_13.pred<-predict(fr_13.lda, final_13.ts) 
-
-names(fr_13.pred)
-fr_13.pclass<-fr_13.pred$class
-
-table(fr_13.pclass, final_13.ts$low_fr_13)
-
-#doing another classification method (quadtratic style)
-fr_13.qda<-qda(low_fr_13~uaf_2013 + uam_2013 + gdp_2013 ,data=final_13.tr)
-
-#getting the results QDA
-fr_13.qda
-
-#getting predictions for QDA
-pred.qda<-predict(fr_13.qda, final_13.ts)
-pqda.c<-pred.qda$class
-table(pqda.c, final_13.ts$low_fr_13 )
-
-#tree analysis 
-tm<-tree(low_fr_13~uaf_2013 + uam_2013 + gdp_2013 ,data=final_13.tr)
-plot(tm)
-text(tm)
-
-prune.mod<-prune.tree(tm,best=5)
-plot(prune.mod)
-text(prune.mod)
-
-#tree analysis with employment differences 
-tm<-tree(low_fr_13~dua_2013 + gdp_2013 ,data=final_13.tr)
-plot(tm)
-text(tm)
-
-prune.mod<-prune.tree(tm,best=6)
-plot(prune.mod)
-text(prune.mod)
-
-summary(tm)
-
 # demo how to add region to data
 country_metadata <- read.csv("femalePrimPers/Metadata_Country_se.prm.prsl.fe.zs_Indicator_en_csv_v2.csv", header=TRUE)
 names(country_metadata)[1] <- "Country.Name"
 fr_ua_13.region <- merge(fr_ua_13,country_metadata[,c(1,3)],by='Country.Name')
-
-
-#regression analysis
-
-#view distributions of variables
-#transform data using logs when necessary
-#check for normality of distributions
-hist(final_13$fr_2013)
-hist(log(final_13$fr_2013))
-hist(final_13$gdp_2013)
-hist(log(final_13$gdp_2013))
-hist(final_13$dua_2013)
-#check for relationship between response and each predictor
-plot(log(final_13$fr_2013),log(final_13$gdp_2013))
-plot(log(final_13$fr_2013),final_13$dua_2013)
-#test for interaction
-plot(log(final_13$gdp_2013),final_13$dua_2013)
-#create regression model
-regmod1<-lm(log(fr_2013)~log(gdp_2013)+dua_2013,data=final_13)
-#find residuals of the model
-res1<-residuals(regmod1)
-#check QQ plot of residuals for normality
-qqnorm(res1)
-#check for equal variance
-plot(res1)
-#view model and model statistics
-regmod1
-summary(regmod1)
